5a538db0dfc536e44be9134aba3b60d587cfb6e4
islamayman <islamwebdevelopment>
2024-10-05 16:09:06 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CarListComponent } from './Components/car-list/car-list.component';
+import { BookingsComponent } from './Components/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'booking',
+        pathMatch: 'full'
+    },
+    {
+        path: "car",
+        component: CarListComponent
+    },
+    {
+        path: 'booking',
+        component: BookingsComponent
+    }
+];
